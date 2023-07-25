48f61839cb27d049c36fd36a3d3758208ac31a12
abhisn15 <abhinugroho41@smp.belajar.id>
2023-07-25 14:51:20 +0700

    ini udh jadi halaman homenya

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,9 +1,12 @@
-
 const Footer = () => {
-    return (
-        <footer className='bg-blue-500 p-20 mt-20'>
-            <h1 className='relative z-10 text-center text-slate-950 text-3xl'>This is the footer.</h1>
-        </footer>
+    return ( <
+        footer className = "bg-blue-500 p-20 mt-20" >
+        <
+        h1 className = "relative z-10 text-center text-slate-950 text-3xl" > { " " }
+        This is the footer. { " " } <
+        /h1>{" "} <
+        h1 > HALLO < /h1>{" "} < /
+        footer >
     );
 };
 
--- src/Router/DesainPerhaiasan/DesainPerhiasan.jsx
+++
@@ -1,9 +0,0 @@
-const DesainPerhiasan = () => {
-    return(
-        <div>
-            <h1 className="text-center mt-[20%] h-[100vh]">INI HALAMAN DESIGN PERHIASAN</h1>
-        </div>
-    )
-}
-
-export default DesainPerhiasan;
